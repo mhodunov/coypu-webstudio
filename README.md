6b72746543f1e4e4bb41825455e7fc64c8525ff9
Maksym Hodunov <m.godunov@yandex.ru>
2021-06-10 13:34:37 +0300

    init: install gulp

--- README.md
+++
@@ -1,2 +0,0 @@
-# coypu-webstudio
-Designing and building landing page for web studio. Working with WordPress.
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/"
+  },
+
+  src: {
+    html: source_folder + "/",
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/index.js",
+    img: source_folder + "/img/**/*.{jpg, png, gif, ico, svg, webp}",
+    fonts: source_folder + "/fonts/*.ttf"
+  },
+
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg, png, gif, ico, svg, webp}"
+  },
+
+  clean: "./" + project_folder + "/"
+};
+
+let { src, dest } = require('gulp'),
+    gulp = require('gulp'),
